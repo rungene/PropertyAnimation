b223644de9a1e8743a4db30a1773d976b25f2292
rungene <rungenesamuel@gmail.com>
2021-03-17 20:28:03 +0300

    Scale the Star

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -19,6 +19,7 @@ package com.google.samples.propertyanimation
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -116,6 +117,50 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun scaler() {
+        //when an object is scaled, it is usually scaled in x and y simultaneously, to avoid making it look
+    // “stretched” along one of the axes
+
+       /* You thus should create an animation that will animate both the SCALE_X and SCALE_Y
+       properties
+       at the same time.*/
+
+       /* There is no single property that scales in both the x and y dimensions, so animations
+        that scale in both x and y need to animate both of these separate properties in parallel.*/
+
+        //The ideal use case for ObjectAnimators which use PropertyValuesHolder parameters is when
+    // you need to animate several properties on the same object in parallel.
+
+       /* use an intermediate object called PropertyValuesHolder to hold this information, and then
+        create a single ObjectAnimator with multiple PropertyValuesHolder objects. This single
+        animator will then run an animation on two or more of these sets of properties/values
+        together.*/
+
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+        //Scaling to a value of 4f means the star will scale to 4 times its default size.
+
+     /*   create an ObjectAnimator object, as before, but use the scaleX and scaleY objects you
+        created above to specify the property/value information*/
+
+        val animator = ObjectAnimator.ofPropertyValuesHolder(star, scaleX, scaleY)
+
+     /*   This is similar to the animators you created previously, but instead of defining a property
+        and a set of values, it uses multiple PropertyValuesHolders which contain all of that
+        information already. Using several PropertyValuesHolder objects in a single animator
+        will cause them all to be animated in parallel.*/
+
+        /*As with the translater() function, you want to make this a repeating/reversing animation
+        to leave the star’s SCALE_X and SCALE_Y properties at their default values (1.0) when
+            the animation is done. Do this by setting the appropriate repeatCount and repeatMode
+                    values on the animator:*/
+
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+       // call the disableViewDurationAnimation() extension function to disable scaleButton
+    // during the animation.
+        animator.disableViewDuringAnimation(scaleButton)
+        animator.start()
+
     }
 
     private fun fader() {
